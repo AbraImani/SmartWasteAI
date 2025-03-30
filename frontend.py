f0279d237eba399a7d134859871307ecbfc77987
Abraham Imani <abrahamfaith325@gmail.com>
2025-03-30 08:36:47 +0200

    Update frontend.py
    Lien vers l'API

--- frontend.py
+++ frontend.py
@@ -13,7 +13,7 @@ c1, c2 = st.columns(2)
 if upload:
     files = {"file" :  upload.getvalue()}
 
-    req = requests.post("https://fastapi-image-z6t7tex4qa-od.a.run.app/predict", files=files)
+    req = requests.post("https://lien_vers_l_APi/predict", files=files)
     resultat = req.json()
     print(resultat)
     rec = resultat["predictions"]
